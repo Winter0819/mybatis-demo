ccd61802133d92584279717b9b6ebc8ab07ce673
e <1017951385@qq.com>
2018-08-05 00:49:05 +0800

    修改介绍文件

--- src/main/java/com/dachen/demo/App.java
+++ src/main/java/com/dachen/demo/App.java
@@ -3,13 +3,15 @@ package com.dachen.demo;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 
 @SpringBootApplication
 @ComponentScan("com.dachen.demo")
 @EnableAsync
-public class App 
+@PropertySource("classpath:application.properties")
+public class App
 {
 	public static void main(String[] args) {
 		new SpringApplicationBuilder(App.class).web(true).run(args);
